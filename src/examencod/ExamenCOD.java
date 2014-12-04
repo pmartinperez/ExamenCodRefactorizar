6cb7c7496af2629fb8dcfd0d9ecd9381a1fadb2f
pmartinperez <pmartinperez@eniac24.danielcastelao.org>
2014-12-04 09:05:54 +0100

    Cambio nombre variables(iN a numero) y metodos (metodoMio a miMetodo)

--- src/examencod/ExamenCOD.java
+++ src/examencod/ExamenCOD.java
@@ -7,21 +7,21 @@ public class ExamenCOD {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int iN = 11;
-        if (metodoMio(iN)) {
+        int numero = 11;
+        if (miMetodo(numero)) {
             System.out.println("Es numero primo");
         } else {
             System.out.println("NO es numero primo");
         }
 
         for (int x = 2; x < 1000; x++) {
-            if (metodoMio(x)) {
+            if (miMetodo(x)) {
                 System.out.print(x + " ");
             }
         }
     }
 
-    public static boolean metodoMio(int variable) {
+    public static boolean miMetodo(int variable) {
         int j = 2;
         boolean aux = true;
         while ((aux) && (j != variable)) {
